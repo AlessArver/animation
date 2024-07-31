c015e45871667daa82e0be5c9f6448af9329696d
Alicia Arver <a.arver@vk.team>
2024-07-31 21:48:50 +0300

    add baseurl

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -4,18 +4,21 @@ import { HomePage } from "../pages/home";
 import { RandomParticlesPage } from "../pages/randomParticles";
 import { PageLayout } from "../layouts/page";
 
-export const router = createBrowserRouter([
-  {
-    element: <PageLayout />,
-    children: [
-      {
-        path: routePaths.home,
-        element: <HomePage />,
-      },
-      {
-        path: routePaths.randomParticles,
-        element: <RandomParticlesPage />,
-      },
-    ],
-  },
-]);
+export const router = createBrowserRouter(
+  [
+    {
+      element: <PageLayout />,
+      children: [
+        {
+          path: routePaths.home,
+          element: <HomePage />,
+        },
+        {
+          path: routePaths.randomParticles,
+          element: <RandomParticlesPage />,
+        },
+      ],
+    },
+  ],
+  { basename: "/animation" }
+);
